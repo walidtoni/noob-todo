fafae030bf7642187970a6fc93c04869943cd6fd
Toni <111330705+walidtoni@users.noreply.github.com>
2024-05-14 12:42:04 +0700

    Add files via upload

---
+++ src/components/TodoCreator.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="input-wrap" :class="{ 'input-err': todo.invalid }">
+    <input type="text" v-model="todo.todo" />
+    <button @click="createTodo()">
+      <slot name="button-content"> Create </slot>
+    </button>
+  </div>
+  <p class="err-msg" v-if="todo.invalid">{{ todo.errMsg }}</p>
+</template>
+
+<script setup>
+import { reactive } from "vue";
+
+const emit = defineEmits(["create-todo"]);
+
+const todo = reactive({
+  todo: "",
+  invalid: false,
+  errMsg: "",
+});
+
+const createTodo = () => {
+  todo.invalid = false;
+  if (todo.todo !== "") {
+    emit("create-todo", todo.todo);
+    todo.todo = "";
+    return;
+  }
+  todo.invalid = true;
+  todo.errMsg = "Cannot be empty!";
+};
+</script>
+
+
+<style lang="scss" scoped>
+.input-wrap {
+  display: flex;
+  transition: 250ms ease;
+  border: 2px solid #494949;
+  border-radius: 10px;
+
+  &.input-err {
+    border-color: red;
+  }
+
+  &:focus-within {
+    box-shadow: 0 -4px 6px -1px rgb(0 0 0 / 0.1),
+      0 -2px 4px -2px rgb(0 0 0 / 0.1);
+  }
+
+  input {
+    width: 100%;
+    padding: 8px 6px;
+    border: none;
+    border-radius: 10px;
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+  button {
+    padding: 8px 16px;
+    border: none;
+    border-radius: 0 8px 8px 0;
+    color: rgb(0, 0, 0);
+    background-color: #f0f0f0;
+    
+    &:hover {
+      color: white;
+      background-color: #818181;
+    }
+  }
+}
+
+.err-msg {
+  margin-top: 6px;
+  font-size: 12px;
+  text-align: center;
+  color: red;
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="about">
+    <h3>About Dino Todo</h3>
+    <br>
+    <p>
+      This is a simple todo app built with Vue 3 and Vite. It uses the
+      composition API and the new <code>setup()</code> syntax, the todos are saved in localstorage(). Also with the help of some extensions. This was my first FE project.
+    </p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    align-items: center;
+    line-height: 1.5;
+    padding: 50vh;
+  }
+}
+
+.about {
+  text-align: center;
+  padding: 25px;
+  line-height: 1.5;
+}
+</style>
